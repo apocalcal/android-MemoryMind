ccf4b4a445512f40f0835dd511b7a9a8cab50b83
chaeh <chaehee15@naver.com>
2024-11-14 02:12:07 +0900

    20241113

--- app/src/main/java/com/example/androidproject1/ApiClient.java
+++ app/src/main/java/com/example/androidproject1/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.219.165:3000/";
+    private static final String BASE_URL = "http://3.38.153.171:3000/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
